1ea26ad0abb9aabf0c302243b69dae77680fb5d8
resteve <resteve@zikzakmedia.com>
2013-04-26 14:24:33 +0200

    pyflakes fixed

--- configuration.py
+++ configuration.py
@@ -1,9 +1,7 @@
 # This file is part product_sale_sequence module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.model import ModelView, ModelSQL, ModelSingleton, fields
-from trytond.pool import Pool, PoolMeta
-from trytond.transaction import Transaction
+from trytond.pool import PoolMeta
 
 __all__ = [
     'ProductConfiguration',
