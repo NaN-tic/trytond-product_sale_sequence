a0578df08dd741a04edee6d168ab54d6e502d757
resteve <resteve@zikzakmedia.com>
2013-04-26 16:57:54 +0200

    Import fields

--- configuration.py
+++ configuration.py
@@ -1,6 +1,7 @@
 # This file is part product_sale_sequence module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
+from trytond.model import fields
 from trytond.pool import PoolMeta
 
 __all__ = [
