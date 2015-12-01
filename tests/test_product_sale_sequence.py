1e6bb5a53902ced4fbc130def38f35a8e5fe16cc
resteve <resteve@zikzakmedia.com>
2015-12-01 11:15:13 +0100

    Tests

--- tests/test_product_sale_sequence.py
+++ tests/test_product_sale_sequence.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part product_sale_sequence module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the product_sale_sequence module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProductSaleSequenceTestCase(unittest.TestCase):
+class ProductSaleSequenceTestCase(ModuleTestCase):
     'Test Product Sale Sequence module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_sale_sequence')
-
-    def test0005views(self):
-        'Test views'
-        test_view('product_sale_sequence')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'product_sale_sequence'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         ProductSaleSequenceTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
